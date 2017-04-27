b6e9eae7ee01da77afbc24de0af079e655516cfb
henry <Henry Warhurst>
2017-04-27 18:23:52 +1000

    Add more comments and licensing stuff

--- Cuda/logisticregression.cuh
+++ Cuda/logisticregression.cuh
@@ -1,3 +1,21 @@
+/*
+ * This file is part of CudaHilbertMaps.
+ *
+ * Copyright (C) 2017 University of Sydney.
+ *
+ * The use of the code within this file and all code
+ * within files that make up the software that is
+ * CudaHilbertMaps is permitted for non-commercial 
+ * purposes only. The full terms and conditions that
+ * apply to the code within this file are detailed 
+ * within the LICENSE.txt file unless explicitly 
+ * stated. By downloading this file you agree to 
+ * comply with these terms.
+ *
+ * Author: Henry Warhurst
+ * 
+ */
+
 #ifndef CUDA_LOGISTICREGRESSION_CUH_
 #define CUDA_LOGISTICREGRESSION_CUH_
 
