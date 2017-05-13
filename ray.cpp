fd5b1dd28410429439d5c11d93e3d68c4298e360
Henry Warhurst <hwar3828@uni.sydney.edu.au>
2017-05-13 10:30:20 +1000

    Ray class compiles

---
+++ ray.cpp
@@ -0,0 +1,41 @@
+#include "ray.h"
+
+Ray::Ray(size_t imageU, size_t imageV)
+{
+	for (size_t rayZ=kStartZ; rayZ<kMaxZ; rayZ+=kStepZ) {
+		float curZ = (float) rayZ / kCorrectionFactor;
+		float curX = (imageU - kOpticalCentreX)*curZ / kFocalLengthX;
+		float curY = (imageV - kOpticalCentreY)*curZ / kFocalLengthY;
+
+		Eigen::Vector3f curPoint;
+		curPoint << curX, curY, curZ;
+		pointsOriginal_.push_back(curPoint);
+	}
+}	
+
+size_t Ray::size() {
+	return pointsOriginal_.size();
+}
+
+void Ray::transformToPose(Eigen::Matrix4f pose)
+{
+	pointsTransformed_ = pointsOriginal_;
+	for (auto &point : pointsTransformed_) {
+		Eigen::Vector4f tmpPoint;
+		tmpPoint << tmpPoint(0), tmpPoint(1), tmpPoint(2), 1;
+		Eigen::Vector4f transformedPoint = pose * tmpPoint;
+		point(0) = transformedPoint(0);
+		point(1) = transformedPoint(1);
+		point(2) = transformedPoint(2);
+	}
+}
+
+void Ray::setSurfaceIntersectionPoint(Eigen::Vector3f surfaceIntersectionPoint)
+{
+	surfaceIntersection_ = surfaceIntersectionPoint;
+}
+
+Eigen::Vector3f Ray::getSurfaceIntersectionPoint() const
+{
+	return surfaceIntersection_;
+}
---
+++ ray.h
@@ -0,0 +1,36 @@
+#ifndef RAY_H_ 
+#define RAY_H_
+
+#include <vector>
+
+#include <eigen3/Eigen/Dense>
+
+class Ray
+{
+	public:
+		Ray(size_t imageU, size_t imageV);
+
+		size_t size();
+
+		void transformToPose(Eigen::Matrix4f pose);
+
+		void setSurfaceIntersectionPoint(Eigen::Vector3f surfaceIntersectionPoint);
+		Eigen::Vector3f getSurfaceIntersectionPoint() const;
+
+	private:
+		std::vector<Eigen::Vector3f> pointsOriginal_;
+		std::vector<Eigen::Vector3f> pointsTransformed_;
+		Eigen::Vector3f surfaceIntersection_;
+
+        static constexpr const double kCorrectionFactor = 1000.0    ;
+        static constexpr const double kFocalLengthX     = 525.0     ;
+        static constexpr const double kFocalLengthY     = 525.0     ;
+        static constexpr const double kOpticalCentreX   = 319.5     ;
+        static constexpr const double kOpticalCentreY   = 239.5     ;
+
+        static constexpr const size_t kMaxZ             = 2000      ;
+        static constexpr const size_t kStepZ            = 200       ;
+        static constexpr const size_t kStartZ           = 100       ;
+};
+
+#endif /* RAY_H_ */
